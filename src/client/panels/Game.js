d0756199f4a8a79d6830c033719e5de0723c59fd
russian-proger <sagitov.2806@mail.ru>
2021-07-23 18:00:12 +0500

    modified: game panel

--- src/client/panels/Game.js
+++ src/client/panels/Game.js
@@ -9,16 +9,25 @@ import { Events } from '../core/Constants';
 import { GamePlay } from '../gameplay';
 
 function GamePanel() {
+  /** @type {React.Ref<HTMLCanvasElement>} */
   const canvasRef = React.createRef(null);
   const [game] = React.useState(new GamePlay());
 
   React.useLayoutEffect(() => {
-    game.setCanvas(canvasRef.current);
+    const canvas = canvasRef.current;
+    game.setCanvas(canvas);
     game.start();
+
+    (window.onresize = () => {
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+    })();
   }, []);
 
   return (
-    <canvas ref={ canvasRef } />
+    <div className="game-panel">
+      <canvas ref={ canvasRef } />
+    </div>
   );
 }
 
